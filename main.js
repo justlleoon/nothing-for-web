60fd318151239f80473266440568648a8fb49c11
lleoon <96005316+justlleoon@users.noreply.github.com>
2024-09-14 13:11:33 +0800

    Minor JQuery Fixes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let counterInterval;
 function startCounter(){
   i = 0;
   $("#startButton").hide()
-  document.getElementById('nothing-counter').innerHTML = "Wait and stay <span>steady</span>...";
+  $('#nothing-counter').html("Wait and stay <span>steady</span>...");
   setTimeout(setAddInterval, 2000);
 }
 
@@ -30,11 +30,11 @@ function addI(){
 	// Adds 1 to the variable "i"
 	i++
   // Changes the text to match with "i"
-  document.getElementById('nothing-counter').innerHTML = `You have been doing <span>Nothing</span> for ${i} seconds.`
+  document.getElementById('#nothing-counter').innerHTML = `You have been doing <span>Nothing</span> for ${i} seconds.`
 }
 
 // A function where if you lose it stops counting "i". 
 function stopAddI(){
 	clearInterval(counterInterval);
-  $("#nothing-counter").text(`You lost and did Nothing for ${i} seconds! Refresh the page to play again.`);
+  $("#nothing-counter").html(`You lost and did <span>Nothing</span> for ${i} seconds! Refresh the page to play again.`);
 }
